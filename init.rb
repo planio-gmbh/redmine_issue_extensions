91a725389ca130acbdc4dc3a04270570e803be30
Takashi Takebayashi <changesworlds@gmail.com>
2010-04-12 22:51:44 +0900

    初期構築

---
+++ init.rb
@@ -0,0 +1,27 @@
+# Issue Extensions plugin for Redmine
+# Copyright (C) 2010  Takashi Takebayashi
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+require 'redmine'
+require 'issue_extensions_issue_hooks'
+
+Redmine::Plugin.register :redmine_issue_extensions do
+  name 'Redmine Issue Extensions plugin'
+  author 'Takashi Takebayashi'
+  description 'This is a Issue Extensions plugin for Redmine'
+  version '0.0.1'
+  requires_redmine :version_or_higher => '0.9.0'
+
+end
---
+++ lib/issue_extensions_issue_hooks.rb
@@ -0,0 +1,36 @@
+# Issue Extensions plugin for Redmine
+# Copyright (C) 2010  Takashi Takebayashi
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+class IssueExtensionsIssueHook < Redmine::Hook::Listener
+  def controller_issues_edit_before_save(context)
+    issue_status_assigned(context)
+  end
+
+private
+  # チケットに担当者が設定されている && 状態が新規の場合、担当に変更する
+  def issue_status_assigned(context)
+    @issue = context[:issue]
+    @issue_status_default = IssueStatus.find(:first, :conditions => ["name = (?)", "新規"])
+    @issue_status_assigned = IssueStatus.find(:first, :conditions => ["name = (?)", "担当"])
+
+    if (@issue[:assigned_to_id] != nil)
+      if (@issue_status_default.id == @issue[:status_id].to_i)
+        @issue[:status_id] = @issue_status_assigned.id.to_s
+        context[:issue] = @issue
+      end
+    end
+  end
+end
