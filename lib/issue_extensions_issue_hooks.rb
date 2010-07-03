55244ec54c9adc45a8b69f968b0f14ceb332c519
Takashi Takebayashi <changesworlds@gmail.com>
2010-07-03 21:51:40 +0900

    fixes #329 遷移元/先管理テーブルと hook での処理の連携完了

--- lib/issue_extensions_issue_hooks.rb
+++ lib/issue_extensions_issue_hooks.rb
@@ -50,13 +50,12 @@ class IssueExtensionsIssueHook < Redmine::Hook::Listener
   # チケットに担当者が設定されている && 状態が新規の場合、担当に変更する
   def issue_status_assigned(context)
     issue = context[:issue]
-    issue_status_default = IssueStatus.find(:first, :conditions => ["name = (?)", '新規'])
-    issue_status_assigned = IssueStatus.find(:first, :conditions => ["name = (?)", '担当'])
+    issue_status = IssueExtensionsStatusFlow.find(:first, :conditions => ['project_id = ?', issue[:project_id].to_i])
 
-    if (issue_status_default.id == issue[:status_id].to_i)
-        issue[:status_id] = issue_status_assigned.id.to_s
+    if (issue_status.old_status_id == issue[:status_id])
+        issue[:status_id] = issue_status.new_status_id
         context[:issue] = issue
-      end if (issue[:assigned_to_id] != nil && issue_status_default != nil && issue_status_assigned != nil)
+      end if (issue[:assigned_to_id] != nil && issue_status != nil)
   end
 
   # チケットがクローズされている場合、進捗を100%に変更する
