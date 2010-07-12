2b653891af8caedae6d9cb7e4e5cc0614ec30cc2
Takashi Takebayashi <changesworlds@gmail.com>
2010-07-12 20:08:52 +0900

    fixes #472, #486 「このチケットに関連するチケットを作成する」のリンクから飛んだ後の関連付けを行う。

--- lib/issue_extensions_issue_hooks.rb
+++ lib/issue_extensions_issue_hooks.rb
@@ -24,89 +24,115 @@ class IssueExtensionsIssueHooks < Redmine::Hook::Listener
 #    end
 #  end
 
-  def controller_issues_edit_before_save(context)
-    issue = context[:issue]
-    project = Project.find(issue[:project_id].to_i)
-    unless (project.module_enabled?(:issue_extensions) == nil)
-      issue_status_assigned(context)
-      issue_status_closed(context)
-      issue_added_watcher(context)
-    end
-  end
+ def controller_issues_edit_before_save context
+   issue = context[:issue]
+   project = Project.find issue[:project_id].to_i
+   unless project.module_enabled? :issue_extensions == nil
+     issue_status_assigned context
+     issue_status_closed context
+     issue_added_watcher context
+   end
+ end
 
-  def controller_issues_bulk_edit_before_save(context)
-    issue = context[:issue]
-    params = context[:params]
-    project = Project.find(issue[:project_id].to_i)
-    unless (project.module_enabled?(:issue_extensions) == nil)
-      # Redmine 0.9.3 では、呼び元で issue[:status_id] に params[:status_id] の値を設定しない為、対応
-      issue[:status_id] = params[:status_id].blank? ? issue[:status_id] : params[:status_id]
-      context[:issue] = issue
-      issue_status_closed(context)
-    end
-  end
+ def controller_issues_bulk_edit_before_save context
+   issue = context[:issue]
+   params = context[:params]
+   project = Project.find issue[:project_id].to_i
+   unless project.module_enabled? :issue_extensions == nil
+     # Redmine 0.9.3 では、呼び元で issue[:status_id] に params[:status_id] の値を設定しない為、対応
+     issue[:status_id] = params[:status_id].blank? ? issue[:status_id] : params[:status_id]
+     context[:issue] = issue
+     issue_status_closed context
+   end
+ end
 
-  private
-  # チケットに担当者が設定されている && 状態が新規の場合、担当に変更する
-  def issue_status_assigned(context)
-    issue = context[:issue]
-    issue_status = IssueExtensionsStatusFlow.find(:first, :conditions => ['project_id = ?', issue[:project_id].to_i])
+ def controller_issues_new_after_save context
+   issue = context[:issue]
+   project = Project.find issue[:project_id].to_i
+   unless project.module_enabled? :issue_extensions == nil
+     issue_added_relation context
+   end
+ end
 
-    if (issue_status.old_status_id == issue[:status_id])
-        issue[:status_id] = issue_status.new_status_id
-        context[:issue] = issue
-      end if (issue[:assigned_to_id] != nil && issue_status != nil)
-  end
+ private
+ # チケットに担当者が設定されている && 状態が新規の場合、担当に変更する
+ def issue_status_assigned context
+   issue = context[:issue]
+   issue_status = IssueExtensionsStatusFlow.find :first, :conditions => ['project_id = ?', issue[:project_id].to_i]
 
-  # チケットがクローズされている場合、進捗を100%に変更する
-  def issue_status_closed(context)
-    issue = context[:issue]
-    issue_status_closed = IssueStatus.find(:all, :conditions =>["is_closed = (?)", true])
+   if issue_status.old_status_id == issue[:status_id]
+       issue[:status_id] = issue_status.new_status_id
+       context[:issue] = issue
+     end if issue[:assigned_to_id] != nil && issue_status != nil
+ end
 
-    issue_status_closed.each {|closed|
-      if (closed.id == issue[:status_id].to_i && issue[:done_ratio] != 100)
-          issue[:done_ratio] = 100
-          context[:issue] = issue
-        end if (issue[:status_id] != nil && issue[:done_ratio] != nil)
-    } unless issue_status_closed.length == 0
-  end
+ # チケットがクローズされている場合、進捗を100%に変更する
+ def issue_status_closed context
+   issue = context[:issue]
+   issue_status_closed = IssueStatus.find :all, :conditions => ["is_closed = (?)", true]
 
-  # チケットを更新した場合、更新者をウォッチャーに追加する
-  def issue_added_watcher(context)
-    issue = context[:issue]
-    journal = context[:journal]
+   issue_status_closed.each {|closed|
+     if closed.id == issue[:status_id].to_i && issue[:done_ratio] != 100
+         issue[:done_ratio] = 100
+         context[:issue] = issue
+       end if issue[:status_id] != nil && issue[:done_ratio] != nil
+   } unless issue_status_closed.length == 0
+ end
 
-    if (Watcher.find(:first, :conditions =>["watchable_type = (?) and watchable_id = (?) and user_id = (?)", journal[:journalized_type], issue[:id].to_i, journal[:user_id].to_i]) == nil)
-        hash_watcher = HashWithIndifferentAccess.new
-        hash_watcher[:user_id]  = journal[:user_id].to_s
-        watcher = Watcher.new(hash_watcher)
-        watcher.watchable_type  = journal[:journalized_type].to_s
-        watcher.watchable_id    = issue[:id].to_i
-        watcher.save
-      end if (journal != nil && journal[:journalized_type] != nil && issue[:id] != nil && journal[:user_id] != nil)
-  end
+ # チケットを更新した場合、更新者をウォッチャーに追加する
+ def issue_added_watcher context
+   issue = context[:issue]
+   journal = context[:journal]
 
-  class IssueExtensionsIssueViewListener < Redmine::Hook::ViewListener
-    def view_issues_show_description_bottom(context)
-      issue = context[:issue]
-      project = context[:project]
-      return '' unless project
-      unless (project.module_enabled?(:issue_extensions) == nil)
-        begin
-          output = "<hr />\n"
-          output << "<div id=\"issue_extensions_relations\">\n"
-#          output << link_to_if_authorized(l(:button_add_relation_issue), {:controller => 'issue_extensions', :action => 'add_relation_issue', :id => @issue}, :class => 'icon icon-move')
-          output << "  " + link_to(l(:label_add_relation_issue), {:controller => 'issue_extensions', :action => 'add_relation_issue', :id => issue.id})
-          output << "</div>\n"
-          return output
-        rescue
-          output = "<hr />\n"
-          output << "<div id=\"issue_extensions_relations\">\n"
-          output << "  error!!\n"
-          output << "</div>\n"
-          return output
-        end
-      end
-    end
-  end
+   if Watcher.find :first, :conditions =>["watchable_type = (?) and watchable_id = (?) and user_id = (?)", journal[:journalized_type], issue[:id].to_i, journal[:user_id].to_i] == nil
+       hash_watcher = HashWithIndifferentAccess.new
+       hash_watcher[:user_id]  = journal[:user_id].to_s
+       watcher = Watcher.new(hash_watcher)
+       watcher.watchable_type  = journal[:journalized_type].to_s
+       watcher.watchable_id    = issue[:id].to_i
+       watcher.save
+     end if journal != nil && journal[:journalized_type] != nil && issue[:id] != nil && journal[:user_id] != nil
+ end
+
+ # 関連チケットを指定している場合、関連付けする
+ def issue_added_relation context
+   issue = context[:issue]
+   params = context[:params]
+   unless params[:relation_issue_id] == nil
+     relation = IssueRelation.new
+     relation.relation_type = IssueRelation::TYPE_RELATES
+     relation.issue_from_id = params[:relation_issue_id]
+     relation.issue_to_id = issue.id
+     relation.save!
+   end
+ end
+
+ class IssueExtensionsIssueViewListener < Redmine::Hook::ViewListener
+   def view_issues_show_description_bottom context
+     issue = context[:issue]
+     project = context[:project]
+     return '' unless project
+     begin
+         output = "<hr />\n"
+         output << "<div id=\"issue_extensions_relations\">\n"
+         output << "  " + link_to(l(:label_add_relation_issue), {:controller => 'issues', :action => 'new', :project_id => project, :relation_issue => issue.id}, :class => 'icon icon-edit' ) + "\n"
+         output << "</div>\n"
+         return output
+       rescue
+       end unless project.module_enabled? :issue_extensions == nil
+   end
+
+   def view_issues_form_details_bottom context
+     project = context[:project]
+     request = context[:request]
+     parameters = request.parameters
+     relation_issue = parameters[:relation_issue]
+     return '' unless relation_issue
+     begin
+         output = hidden_field_tag :relation_issue_id, relation_issue
+         return output
+       rescue
+       end unless project.module_enabled? :issue_extensions == nil
+   end
+ end
 end
