462e05ea0c6f3d57ef8ca423ad293fe673de4652
Takashi Takebayashi <changesworlds@gmail.com>
2010-07-23 01:27:36 +0900

    Correction of test code(fixtures -> Mocha)

--- test/unit/issue_extensions_status_flow_test.rb
+++ test/unit/issue_extensions_status_flow_test.rb
@@ -16,21 +16,35 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require File.dirname(__FILE__) + '/../test_helper'
 
-class IssueExtensionsStatusFlowTest < ActiveSupport::TestCase
-  fixtures :issue_extensions_status_flows
+class IssueExtensionsStatusFlowTest < Test::Unit::TestCase
+  def a_issue_extensions_status_flow
+    IssueExtensionsStatusFlow.create! :project_id => 1,
+      :tracker_id => 0,
+      :old_status_id => 1,
+      :new_status_id => 2,
+      :updated_by => 1
+  end
 
   context 'a IssueExtensionsStatusFlow instance' do
+    setup do
+      a_issue_extensions_status_flow
+    end
+
     should "find return not nil" do
       assert_not_nil IssueExtensionsStatusFlow.find :first
     end
 
     should "find_or_create return project_id 5" do
       assert !IssueExtensionsStatusFlow.find(:first, :conditions => 'project_id = 5')
-      setting = IssueExtensionsStatusFlow.find_or_create 5, 1
-      assert_equal 5, setting.project_id
+      issue_extensions_status_flow = IssueExtensionsStatusFlow.find_or_create 5, 1
+      assert_equal 5, issue_extensions_status_flow.project_id
       assert IssueExtensionsStatusFlow.find :first, :conditions => 'project_id = 5'
-      setting = IssueExtensionsStatusFlow.find_or_create 5, 1
-      assert_equal 5, setting.project_id
+      issue_extensions_status_flow = IssueExtensionsStatusFlow.find_or_create 5, 1
+      assert_equal 5, issue_extensions_status_flow.project_id
+    end
+
+    teardown do
+      IssueExtensionsStatusFlow.delete_all
     end
   end
 end
