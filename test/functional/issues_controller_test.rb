bf740859119e8301579d154a7bf67eaffbd83bf1
Takashi Takebayashi <changesworlds@gmail.com>
2010-07-24 22:20:25 +0900

    Addition of test code

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -49,14 +49,14 @@ class IssuesControllerTest < ActionController::TestCase
     @controller = IssuesController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
-    User.current = nil
+    EnabledModule.generate! :project_id => 1, :name => 'issue_extensions'
+    @request.session[:user_id] = 2
   end
 
   context "#create" do
-    should "accept get with relation issue" do
-      @request.session[:user_id] = 2
+    should "accept post with relation issue" do
       assert_difference 'Issue.count' do
-        post :create,:project_id => 1,
+        post :create, :project_id => 1,
           :issue => {:tracker_id => 3,
                      :status_id => 2,
                      :subject => 'This is the test_new issue',
@@ -65,7 +65,7 @@ class IssuesControllerTest < ActionController::TestCase
                      :estimated_hours => ''}
       end
       assert_difference 'Issue.count' do
-        post :create,:project_id => 1,
+        post :create, :project_id => 1,
           :relation_issue_id => Issue.last.id,
           :issue => {:tracker_id => 3,
                      :status_id => 2,
@@ -80,6 +80,29 @@ class IssuesControllerTest < ActionController::TestCase
       assert_equal issue_relation.relation_type, IssueRelation::TYPE_RELATES
       assert_equal Issue.last.id - 1, issue_relation.issue_from_id
       assert_equal Issue.last.id, issue_relation.issue_to_id
+    end
+  end
+
+  context "#update" do
+    should "accept post with done_ratio 100 and watcher" do
+      assert_difference 'Issue.count' do
+        post :create, :project_id => 1,
+          :issue => {:tracker_id => 3,
+                     :status_id => 2,
+                     :subject => 'This is the test_new issue',
+                     :description => 'This is the description',
+                     :priority_id => 5,
+                     :estimated_hours => ''}
+      end
+      put :update, :id => Issue.last.id,
+        :issue => {:status_id => 5,
+                   :priority_id => 4}
+      assert_redirected_to :action => 'show', :id => Issue.last.id
+      issue = Issue.find Issue.last.id
+      assert_not_nil issue
+      assert_equal 5, issue.status_id
+      assert_equal 4, issue.priority_id
+      assert_equal 100, issue.done_ratio
     end
   end
 end
