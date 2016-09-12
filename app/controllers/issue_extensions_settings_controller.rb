26e3c4de8b77acc02e7af4c879555162216edfad
Jens Kraemer <jk@jkraemer.net>
2016-09-12 18:14:34 +0800

    fix settings controller for Rails 4

--- app/controllers/issue_extensions_settings_controller.rb
+++ app/controllers/issue_extensions_settings_controller.rb
@@ -15,17 +15,21 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 class IssueExtensionsSettingsController < ApplicationController
-  # アクションが呼ばれる前に呼ぶメソッド
+  before_filter :find_project
   before_filter :authorize
 
   def update
     @user = User.current
     @issue_extension_status_flow = IssueExtensionsStatusFlow.find_or_create(@project.id, @user.id)
-    @issue_extension_status_flow.attributes = params[:setting]
+    @issue_extension_status_flow.attributes = issue_extension_settings_params
     @issue_extension_status_flow.updated_by = @user.id
     @issue_extension_status_flow.save!
     flash[:notice] = l(:notice_successful_update)
     redirect_to :controller => 'projects', :action => "settings", :id => @project, :tab => 'issue_extensions'
+  end
+
+  def issue_extension_settings_params
+    params.require(:setting).permit :old_status_id, :new_status_id
   end
 
 end
